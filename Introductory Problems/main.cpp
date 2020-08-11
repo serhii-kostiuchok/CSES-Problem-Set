88c7f0e17bed3e93664f44c9d00f86fca31786ea
Serhii Kostiuchok <serhii-kostiuchok@ukr.net>
2020-08-11 22:45:46 +0300

    Внесення змін до першого завдання...

--- Introductory Problems/main.cpp
+++ Introductory Problems/main.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 int main()
 {
-	unsigned long n;
-	cin >> n;
+    unsigned long n;
+    cin >> n;
 
-	while (n != 1)
-	{
-		cout << n << ' ';
+    while (n != 1)
+    {
+        cout << n << ' ';
 
-		if (n % 2 == 0)
-			n = n / 2;
-		else
-			n = n * 3 + 1;
-	}
+        if (n % 2 == 0)
+            n = n / 2;
+        else
+            n = n * 3 + 1;
+    }
 
-	cout << n << endl;
+    cout << n << endl;
 
-	return 0;
+    return 0;
 }
