859e5727e941445dc2af2bb415574036538d3b3b
Serhii Kostiuchok <serhii-kostiuchok@ukr.net>
2020-08-11 22:40:01 +0300

    Вирішення першої проблеми

---
+++ Introductory Problems/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	unsigned long n;
+	cin >> n;
+
+	while (n != 1)
+	{
+		cout << n << ' ';
+
+		if (n % 2 == 0)
+			n = n / 2;
+		else
+			n = n * 3 + 1;
+	}
+
+	cout << n << endl;
+
+	return 0;
+}
